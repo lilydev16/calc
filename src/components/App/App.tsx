3b7b133cb866196065dd9895b8f0df3ce76b0054
lswatson16 <lauralynw16@gmail.com>
2022-09-07 19:51:41 -0400

    Update App path and add state to store an array of calculations

---
+++ src/components/App/App.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useState } from 'react';
+import './App.css';
+import Calculator from '../Calculator/Calculator'
+import List from '../List/List'
+
+export interface Calculation {
+  a: number, 
+  b: number, 
+  operator: string; 
+  total: number
+}
+
+function App() {
+  // const [calculations, setCalculations] = useState<{a: number, b: number, operator: string; result: number}[]>([])
+  const [calculations, setCalculations] = useState<Calculation[]>([])
+
+  const addCalc = (entry: Calculation) => {
+    setCalculations([...calculations, entry])
+  }
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Calculator</h1>
+      </header>
+      <div>
+        <p>Calculate the total</p>
+        <Calculator addCalc={addCalc} />
+        <List calculations={calculations} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
